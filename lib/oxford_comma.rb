641cea8396acd4b5f8550216ecb41fc00488e207
Kayla Kingston <kkingston09@gmail.com>
2020-05-31 21:24:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,7 @@ def oxford_comma(array)
     array[0]
   elsif array.length == 2 
     array[0] + " and " + array[1]
-  
+  elsif array.length == 3
+    array[0] + ", " + array[1] + ", and " + array[2]
   end
 end
